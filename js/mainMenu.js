806d1d033094ecdf794ecef2220da6ff5aa578fa
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-06-06 19:20:53 -0400

    Directory Restructure

---
+++ js/mainMenu.js
@@ -0,0 +1,56 @@
+/*
+MAINMENU.JS FOR WEAKEST LINK EMULATION
+BY: ADAM STINZIANI
+EMAIL: astinziani@myseneca.ca
+DATE: 2019-05-30
+*/
+const saveBut = document.querySelector("#save");
+const thefirst = document.querySelector("#firstName");
+let fn = document.createElement("h1");
+
+var begin = document.createElement("a");
+begin.innerHTML = "BEGIN!!!";
+begin.setAttribute("href", "../html/game.html");
+
+saveBut.addEventListener("click", save);
+saved = localStorage.getItem("Saved");
+//CHECK FOR SAVED STATE AND ADD OPTION TO CONTINUE
+if (saved) {
+    first = localStorage.getItem("firstName");
+    last = localStorage.getItem("lastName");
+    info = document.createElement("h1");
+    info.innerHTML = `CONTINUE PLAYING AS ${first} ${last}`;
+    center.appendChild(info);
+    let continueGame = document.createElement("a");
+    continueGame.setAttribute("href", "../html/game.html");
+    continueGame.setAttribute("class", "continueGame");
+    continueGame.style.color = "white";
+    continueGame.style.fontSize = "300%";
+    continueGame.style.textDecoration = "none";
+    continueGame.innerHTML = `CONTINUE`;
+    center.appendChild(continueGame);
+}
+function save() {
+    const first = document.querySelector("#fn").value;
+    const last = document.querySelector("#ln").value;
+    const age = document.querySelector("#age").value;
+    const gender = document.querySelector("#sex").value;
+    //CAPTURE AND VALIDATE USER INFORMATION
+    if (first == "" || last == "" || age == "") {
+        alert(`Please enter all of the values`);
+    }
+    else {
+        localStorage.setItem("Saved", false);
+        begin.style.color = "white";
+        begin.style.fontSize = "300%";
+        begin.style.position = "relative";
+        begin.style.right = "70px";
+        container.appendChild(begin);
+        alert(`Welcome to the game ${first} ${last}, a ${gender} of age ${age}!!!`);
+        localStorage.setItem("firstName", first);
+        localStorage.setItem("lastName", last);
+        localStorage.setItem("age", age);
+        localStorage.setItem("gender", gender);
+    }
+}
+
--- server.js
+++ server.js
@@ -1,39 +1,33 @@
-/*
-SERVER.JS FOR WEAKEST LINK EMULATION
-BY: ADAM STINZIANI
-EMAIL: astinziani@myseneca.ca
-DATE: 2019-05-30
-*/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require("path");
 
-app.use(express.static(__dirname + 'img')); //Serves resources from img folder
-
+app.use(express.static("css")); //Serves resources from css folder
+app.use(express.static("js")); //Serves resources from js folder
 
 // setup a 'route' to listen on the default url path
 app.get("/index.html", (req, res) => {
-    res.sendFile(path.join(__dirname,"/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"/index.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.get("/html/rules.html", (req, res) => {
-    res.sendFile(path.join(__dirname,"/html/rules.html"));
+    res.sendFile(path.join(__dirname, "/html/rules.html"));
 });
 
 app.get("/html/mainMenu.html", (req, res) => {
-    res.sendFile(path.join(__dirname,"/html/mainMenu.html"));
+    res.sendFile(path.join(__dirname, "/html/mainMenu.html"));
 });
 
 app.get("/html/game.html", (req, res) => {
-    res.sendFile(path.join(__dirname,"/html/game.html"));
+    res.sendFile(path.join(__dirname, "/html/game.html"));
 });
 
 app.get("/html/api.html", (req, res) => {
-    res.sendFile(path.join(__dirname,"/html/api.html"));
+    res.sendFile(path.join(__dirname, "/html/api.html"));
 });
 app.listen(HTTP_PORT);
